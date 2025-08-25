a16cf868e15c603d29aa9594ea05ec83c3a8e893
crazycorvus <user@example.com>
2025-08-26 00:53:54 +0300

    add

---
+++ src/ai_dial_interceptor_google/app.py
@@ -0,0 +1,18 @@
+from aidial_interceptors_sdk.examples.app_factory import create_app
+from aidial_interceptors_sdk.utils._env import get_env
+from aidial_interceptors_sdk.utils._http_client import get_http_client
+
+from .registry import INTERCEPTORS
+
+dial_url = get_env("DIAL_URL")
+
+
+async def client_factory():
+    return get_http_client()
+
+
+app = create_app(
+    dial_url=dial_url,
+    client_factory=client_factory,
+    interceptors=INTERCEPTORS,
+)
