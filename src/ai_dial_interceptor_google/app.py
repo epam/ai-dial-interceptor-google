2a211420f1d54f3caa8d4940abb8dd9151e6f5e7
crazycorvus <user@example.com>
2025-09-09 13:00:19 +0300

    fix interceptors

--- src/ai_dial_interceptor_google/app.py
+++ src/ai_dial_interceptor_google/app.py
@@ -1,6 +1,7 @@
-from aidial_interceptors_sdk.examples.app_factory import create_app
 from aidial_interceptors_sdk.utils._env import get_env
 from aidial_interceptors_sdk.utils._http_client import get_http_client
+
+from ai_dial_interceptor_google.app_factory import create_app
 
 from .registry import INTERCEPTORS
 
